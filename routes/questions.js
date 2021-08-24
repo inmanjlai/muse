30724301b4062202eca13b13c7fe0e7e89a6ebbd
grantc00 <grantcky@gmail.com>
2021-08-24 09:48:02 -0700

    question route started

---
+++ routes/questions.js
@@ -0,0 +1,16 @@
+const express = require('express');
+var router = express.Router();
+const { Question } = require('../db/models')
+
+router.get('/', (req, res) => {
+    res.render('question', { Question })
+})
+
+router.get('/new', (req, res) => {
+    res.render('newquestion', { Question })
+})
+
+
+
+
+module.exports = router;
