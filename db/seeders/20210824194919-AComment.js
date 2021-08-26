e7f6161f9cb354139245b7ade307a2dcfb470073
aryan <aryanattula@gmail.com>
2021-08-26 13:29:40 -0400

    test

--- db/seeders/20210824194919-AComment.js
+++ db/seeders/20210824194919-AComment.js
@@ -2,9 +2,11 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('AComments', [
-      {}
-    ], {});
+    return queryInterface.bulkInsert('AComments', [  
+    { comment: 'She deserves this. Good riddance.', answer_id: 1, user_id: 4, createdAt: new Date(), updatedAt: new Date() },  
+    { comment: 'This is so sad.', answer_id: 1, user_id: 6, createdAt: new Date(), updatedAt: new Date() },
+    { comment: 'Who?', answer_id: 1, user_id: 3, createdAt: new Date(), updatedAt: new Date() }     
+    ], {});  
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -17,14 +19,8 @@ module.exports = {
     */
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('People', null, {});
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+  down: (queryInterface, Sequelize) => { 
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('AComments', null, { truncate: true, cascade: true, restartIdentity: true });   
   }
 };
