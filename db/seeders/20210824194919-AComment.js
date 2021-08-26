e318b1e700a7a0e53e43836707e6bcc762f0c09f
Ismail <inmanjlai@hotmail.com>
2021-08-26 13:27:29 -0400

    fixed grid styling to include footer

--- db/seeders/20210824194919-AComment.js
+++ db/seeders/20210824194919-AComment.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('People', null, {});
+    return queryInterface.bulkDelete('AComments', null, {});
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
