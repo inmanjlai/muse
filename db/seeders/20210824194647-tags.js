c1f1df81151c4c16edef6a85c6faad828733ab2e
grantc00 <grantcky@gmail.com>
2021-08-26 10:14:42 -0700

    description spelling fixed

--- db/seeders/20210824194647-tags.js
+++ db/seeders/20210824194647-tags.js
@@ -3,15 +3,15 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Tags', [
-      {name: 'songs', desciption: 'A specific song/genre/album', createdAt: new Date(), updatedAt: new Date()},
-      {name: 'artists', desciption: 'A specific person/group within the music industry', createdAt: new Date(), updatedAt: new Date()},
-      {name: 'k-pop', desciption: '<3 KPOP', createdAt: new Date(), updatedAt: new Date()},
-      {name: 'concerts', desciption: 'lfg concert', createdAt: new Date(), updatedAt: new Date()},
-      {name: 'technology', desciption: 'newest tech', createdAt: new Date(), updatedAt: new Date()},         
-    ], {});  
-  },      
+      {name: 'songs', description: 'A specific song/genre/album', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'artists', description: 'A specific person/group within the music industry', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'k-pop', description: '<3 KPOP', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'concerts', description: 'lfg concert', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'technology', description: 'newest tech', createdAt: new Date(), updatedAt: new Date()},
+    ], {});
+  },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Tags', null, { truncate: true, cascade: true, restartIdentity: true }); 
+    return queryInterface.bulkDelete('Tags', null, { truncate: true, cascade: true, restartIdentity: true });
 }
-} 
+}
