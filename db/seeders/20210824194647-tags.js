9e7305a52ac041fdfe1a8cb2a37f3f7e106dc398
Ismail <inmanjlai@hotmail.com>
2021-08-26 13:19:36 -0400

    fixed

--- db/seeders/20210824194647-tags.js
+++ db/seeders/20210824194647-tags.js
@@ -3,11 +3,11 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Tags', [
-      {name: 'songs', description: 'A specific song/genre/album', createdAt: new Date(), updatedAt: new Date()},
-      {name: 'artists', description: 'A specific person/group within the music industry', createdAt: new Date(), updatedAt: new Date()},
-      {name: 'k-pop', description: '<3 KPOP', createdAt: new Date(), updatedAt: new Date()},
-      {name: 'concerts', description: 'lfg concert', createdAt: new Date(), updatedAt: new Date()},
-      {name: 'technology', description: 'newest tech', createdAt: new Date(), updatedAt: new Date()},         
+      {name: 'songs', desciption: 'A specific song/genre/album', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'artists', desciption: 'A specific person/group within the music industry', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'k-pop', desciption: '<3 KPOP', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'concerts', desciption: 'lfg concert', createdAt: new Date(), updatedAt: new Date()},
+      {name: 'technology', desciption: 'newest tech', createdAt: new Date(), updatedAt: new Date()},         
     ], {});  
   },      
 
